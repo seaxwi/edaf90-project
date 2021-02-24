c4e07060f8d948261ac4e440c59ce6521ef0e8dc
JohanHakansson <johanh98@live.com>
2021-02-24 15:35:29 +0100

    Added form for creating a custom quiz.

--- src/app/quiz-form/quiz-form.component.ts
+++ src/app/quiz-form/quiz-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Quiz } from './quiz';
+
 @Component({
   selector: 'app-quiz-form',
   templateUrl: './quiz-form.component.html',
@@ -7,7 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizFormComponent implements OnInit {
 
+  categories = [
+    'Any Category', 'General Knowledge', 'Entertainment: Books',
+    'Entertainment: Film', 'Entertainment: Music', 'Entertainment: Musicals & Theatres',
+    'Entertainment: Television', 'Entertainment: Video Games', 'Entertainment: Board Games',
+    'Science & Nature', 'Science: Computers', 'Science: Mathematics',
+    'Mythology', 'Sports', 'Geography',
+    'History', 'Politics', 'Art',
+    'Celebrities', 'Animals'
+  ];
+
+  difficulties = [
+    'Any Difficulty', 'Easy',
+    'Medium', 'Hard'
+  ];
+
+  types = [
+    'Any Type', 'Multiple Choice', 'True / False'
+  ];
+
+  model = new Quiz(10, this.categories[0], this.difficulties[0], this.types[0]);
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+
   ngOnInit(): void {
+
   }
 
 }
---
+++ src/app/quiz-form/quiz.ts
@@ -0,0 +1,10 @@
+export class Quiz {
+
+  constructor(
+    public nbrQuestions : number,
+    public category : string,
+    public difficulty : string,
+    public type : string
+  ) {  }
+
+}
